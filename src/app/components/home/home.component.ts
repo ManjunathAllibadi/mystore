2205494cd523743e6164e610bc328de95ff096e4
manjunath <manjunathcallibadi@gmail.com>
2022-01-14 14:34:59 +0530

    items added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,6 +10,360 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
   }
+
+  electronicItems = [];
+  groceryItems = [
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""}
+  ];
+  mobiles = [
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""}
+  ];
+  headphones = [
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""}
+  ];
+  laptops = [
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""}
+  ];
+  televisions = [
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""}
+  ];
+  cameras = [
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""}
+  ];
+  computerPeripherals = [
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""}
+  ];
+  musicalInstruments = [
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""}
+  ];
+  mensFashion = [
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""}
+  ];
+  womenFashion = [
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""}
+  ];
+  kidsFashion = [
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""}
+  ];
+  furnitures = [
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""}
+  ];
+  homeAccessories = [
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""}
+  ];
+  sportsItems = [
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""}
+  ];
+  others = [
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""},
+    {item:""}
+  ];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
